47a826aedeac2392cdb3ebdbea041caabe6d1c5c
Najia Afrin <najiaafrin2@gmail.com>
2025-05-25 23:16:33 +0200

    setup homepage

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,20 +1,3 @@
-<script setup>
-
-</script>
-
 <template>
-  <header>
-
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
   <RouterView />
 </template>
-
-<style scoped>
-</style>
