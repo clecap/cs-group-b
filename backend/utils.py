293cb65a25e770d961ff40352718f70bb6752612
Sid <siddharthbisht99@gmail.com>
2025-06-09 18:28:01 +0200

    use config file, add db access function, add '/user/register' path in backend

--- backend/utils.py
+++ backend/utils.py
@@ -1,4 +1,11 @@
+from configparser import ConfigParser
 from Crypto.Util import number
+import sqlite3
+from flask import g
+
+
+config_object = ConfigParser()
+config_object.read("config.ini")
 
 
 def generate_blum(required_bits):
@@ -16,3 +23,22 @@ def generate_blum(required_bits):
             continue
 
         return result
+
+
+database_info = config_object["DATABASE"]
+
+
+def get_db():
+    db = getattr(g, "_database", None)
+    if db is None:
+        db = g._database = sqlite3.connect(database_info["url"])
+        db.row_factory = sqlite3.Row
+        cur = db.cursor()
+
+        if (
+            cur.execute("SELECT name FROM sqlite_master WHERE name='user'").fetchone()
+            is None
+        ):
+            cur.execute("CREATE TABLE user(username PRIMARY KEY, pubKeys, blum)")
+
+    return db
