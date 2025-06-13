891eaf7db90970f0e6a6e7f15858079bb197b141
Najia Afrin <najiaafrin2@gmail.com>
2025-06-13 14:14:07 +0200

    fix port for socket

--- frontend/src/helpers/socket.js
+++ frontend/src/helpers/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const SOCKET_URL =  import.meta.env.PROD === "production" ? undefined : "http://localhost:5000";
+const SOCKET_URL =  import.meta.env.PROD === "production" ? undefined : "http://localhost:8000";
 
 
 const socket = io(SOCKET_URL);
