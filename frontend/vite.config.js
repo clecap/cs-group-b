675bb20e88839a2b6aabd04e1c957e3316b3059e
Konstantin Frunzek <konstantin.frunzek@uni-rostock.de>
2025-06-23 14:44:44 +0200

    added the url of our server.. otherwise the connection is not possible ( this was cause by some of the last changes..)

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -17,4 +17,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server: {
+    allowedHosts: ['feigefiatshamirdemo.ddns.net', 'localhost'],
+  }
 })
