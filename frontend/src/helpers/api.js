8ded2ba1c51fbc19d2405aec163509348e98a0d8
Najia Afrin <najiaafrin2@gmail.com>
2025-07-11 19:54:40 +0200

    add api url for production mode

--- frontend/src/helpers/api.js
+++ frontend/src/helpers/api.js
@@ -2,8 +2,7 @@ import axios from 'axios';
 
 // Create an Axios instance
 const api = axios.create({
-  // baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000',
-  baseURL: 'https://feigefiatshamirdemo.ddns.net:8000',
+  baseURL: import.meta.env.PROD ?  'https://feigefiatshamirdemo.ddns.net:8000': "http://localhost:8000",
   timeout: 10000, // 10 seconds
   headers: {
     'Content-Type': 'application/json',
