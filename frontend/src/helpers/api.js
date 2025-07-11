38bffed5893961b0c9f6bdd373de4b46548a5709
Sid <siddharthbisht99@gmail.com>
2025-07-11 11:50:24 +0200

    correction for temp change

--- frontend/src/helpers/api.js
+++ frontend/src/helpers/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // Create an Axios instance
 const api = axios.create({
   // baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000',
-  baseURL: 'https://feigefiatshamirdemo.ddns.net:8000'
+  baseURL: 'https://feigefiatshamirdemo.ddns.net:8000',
   timeout: 10000, // 10 seconds
   headers: {
     'Content-Type': 'application/json',
